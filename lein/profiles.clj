18e83a3520872ced608927a1ce7686e4e0b11d83
Marcel Ochs <marcel.ochs@vimcar.com>
2019-01-29 17:07:19 +0100

    Remove ale and add syntastic for better syntax support and also add settings for clojure and python formatters and linters

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-kibit "0.1.5"]]
-        :dependencies [[cljfmt "0.5.1"]]
+{:user {:plugins [[jonase/eastwood "0.3.5"]]
+        :dependencies [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                       [cljfmt "0.5.1"]] 
         :repl-options {:init (require 'cljfmt.core)}}}

6613793613ea4456b8bb1f3becf102091ed0b112
Marcel Ochs <marcel.ochs@vimcar.com>
2019-01-29 16:20:28 +0100

    Add more configs to project.clj and change ale error and warning sign

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,3 @@
-{
-  :user {:plugins [[lein-kibit "0.1.5"]]
-         :dependencies [[cljfmt "0.5.1"]]}
-}
+{:user {:plugins [[lein-kibit "0.1.5"]]
+        :dependencies [[cljfmt "0.5.1"]]
+        :repl-options {:init (require 'cljfmt.core)}}}
